896627f5bfd2a4db7c0b421ab08edc1b85785654
unknown <fatimah.azzhr@gmail.com>
2018-02-07 14:30:23 +0700

    release 0

--- nested_array_2_convert.js
+++ nested_array_2_convert.js
@@ -12,10 +12,19 @@ let roster = [
 // [[roster[0][0], roster[1][0]], [roster[0][1], roster[1][1]],...
 
 function convert_roster_format (nestedArray) {
-  // your convert code here
+  let result = [];
+  for (var i = 0; i < roster.length; i++) {
+    var obj = {};
+    for (var j = 0; j < roster[0].length; j++) {
+      obj[roster[0][j]] = roster[i][j];
+    }
+    result.push(obj);
+  }
+  result.shift();
+  return result;
 }
 
-let object_roster = convert_roster_format(roster)
+let object_roster = convert_roster_format(roster);
 console.log(object_roster[2])
 
 // => { "Number": 31, "Name": "Harvey Kay", "Position": "Shooting Guard", "Points per Game": [0, 30, 16, 0, 25] }
