8859b11116c06d0f534a1ca61ddba35ee2153627
Kevin Himawan Hie <kevinhimawanhie@Kevins-MacBook-Pro.local>
2018-02-07 14:15:00 +0700

    sudah selesai

--- nested_array_2_convert.js
+++ nested_array_2_convert.js
@@ -1,5 +1,31 @@
 'use strict'
 
+class NestedArrayToObject{
+  constructor(){
+    this.hasil = []
+  }
+
+  convert_roster_format(rawData){
+   
+    for(let i = 1;i < rawData.length;i++){
+      let obj = {}
+      for(let j in rawData[i]){
+        obj[rawData[0][j]] = rawData[i][j]
+      }
+      this.hasil.push(obj)
+    }
+  }
+
+  checkName (value){
+    for(let i of this.hasil){
+      if(i.Name === value){
+        return true
+      }
+    }
+    return false
+  }
+}
+
 let roster = [
   ['Number', 'Name', 'Position', 'Points per Game'],
   [12, 'Joe Schmo', 'Center', [14, 32, 7, 0, 23] ],
@@ -9,19 +35,20 @@ let roster = [
   [22, 'MK DiBoux ', 'Small Forward ', [11, 0, 23, 17, 0] ]
 ]
 
-// [[roster[0][0], roster[1][0]], [roster[0][1], roster[1][1]],...
+let release = new NestedArrayToObject()
+release.convert_roster_format(roster)
+console.log(release.checkName('Joe Schmo'))
 
-function convert_roster_format (nestedArray) {
-  // your convert code here
-}
 
-let object_roster = convert_roster_format(roster)
-console.log(object_roster[2])
+// let object_roster = convert_roster_format(roster)
+// console.log(object_roster[2])
 
 // => { "Number": 31, "Name": "Harvey Kay", "Position": "Shooting Guard", "Points per Game": [0, 30, 16, 0, 25] }
 
-console.log(object_roster[0]['Name'] == 'Joe Schmo') // outputs true
+// console.log(object_roster[0]['Name'] == 'Joe Schmo') // outputs true
 
-module.exports = {
-  convert_roster_format
-}
+
+
+// module.exports = {
+//   convert_roster_format
+// }
