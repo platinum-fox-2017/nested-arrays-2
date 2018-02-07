ce07cb7e740cc3c295d2a148413ed4325d531519
agnynureza <rezaagny@gmail.com>
2018-02-07 16:34:55 +0700

    release 0

--- nested_array_2_convert.js
+++ nested_array_2_convert.js
@@ -12,10 +12,19 @@ let roster = [
 // [[roster[0][0], roster[1][0]], [roster[0][1], roster[1][1]],...
 
 function convert_roster_format (nestedArray) {
-  // your convert code here
+  let result = []
+  for(let i = 1  ; i < nestedArray.length ; i++){
+    let obj = {}
+    for(let j =0 ; j < nestedArray[0].length ;j++){
+      obj[nestedArray[0][j]] =nestedArray[i][j]
+    }
+    result.push(obj)
+  }
+  return result
 }
 
 let object_roster = convert_roster_format(roster)
+
 console.log(object_roster[2])
 
 // => { "Number": 31, "Name": "Harvey Kay", "Position": "Shooting Guard", "Points per Game": [0, 30, 16, 0, 25] }
